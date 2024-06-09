2ca4be2684830ca19607488e13ae9f76f2bc2ba9
Akshola00 <akinniyishola07@gmail.com>
2024-06-09 14:36:14 +0100

    Changes to be committed: 	modified:   CV_generator/__pycache__/settings.cpython-311.pyc 	modified:   base/__pycache__/models.cpython-311.pyc 	new file:   base/migrations/0001_initial.py 	new file:   base/migrations/0002_alter_profile_phone.py 	new file:   base/migrations/__pycache__/0001_initial.cpython-311.pyc 	new file:   base/migrations/__pycache__/0002_alter_profile_phone.cpython-311.pyc 	modified:   base/models.py 	modified:   db.sqlite3

--- base/models.py
+++ base/models.py
@@ -4,11 +4,11 @@ from django.db import models
 class profile(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField(max_length=200)
-    phone = models.BigIntegerField(max_length=200)
+    phone = models.CharField(max_length=200)
     summary = models.TextField(max_length=2000)
     degree = models.TextField(max_length=200)
     school = models.TextField(max_length=200)
     university = models.TextField(max_length=200)
-    previous_work = models.TextField(max_length=200)
-    skills = models.TextField(max_length=100)
+    previous_work = models.TextField(max_length=2000)
+    skills = models.TextField(max_length=1000)
 
