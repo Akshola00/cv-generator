a97e243d1ec1449f7e6f37c4c080c6ee8e0c77d8
Akshola00 <akinniyishola07@gmail.com>
2024-06-09 14:35:57 +0100

    Changes to be committed: 	modified:   CV_generator/settings.py 	modified:   base/models.py 	modified:   db.sqlite3

--- base/models.py
+++ base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class profile(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    phone = models.BigIntegerField(max_length=200)
+    summary = models.TextField(max_length=2000)
+    degree = models.TextField(max_length=200)
+    school = models.TextField(max_length=200)
+    university = models.TextField(max_length=200)
+    previous_work = models.TextField(max_length=200)
+    skills = models.TextField(max_length=100)
+
